e410f5a4e5294989532271bda04cb77f447261b5
anmolonruby <anmol1771@gmail.com>
2015-07-18 17:16:41 +0530

    initial setup
    started cordova project, added fireboxes platform, added plugins - gps,
    accelerometer and network connection

---
+++ platforms/firefoxos/www/cordova_plugins.js
@@ -0,0 +1,84 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/src/firefoxos/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer-impl",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/src/firefoxos/GeolocationProxy.js",
+        "id": "org.apache.cordova.geolocation.GeolocationProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/src/firefoxos/NetworkProxy.js",
+        "id": "org.apache.cordova.network-information.NetworkProxy",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.device-motion": "0.2.11",
+    "org.apache.cordova.geolocation": "0.3.12",
+    "org.apache.cordova.network-information": "0.2.15"
+}
+// BOTTOM OF METADATA
+});
